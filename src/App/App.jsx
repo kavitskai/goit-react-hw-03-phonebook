0b7dc90013e2f0bb7eaeb770f7a982c6920852cf
kavitskai <kavitskai@gmail.com>
2021-08-10 01:36:31 +0300

    HW-03

---
+++ src/App/App.jsx
@@ -0,0 +1,90 @@
+import { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import contacts from "../data/contacts.json";
+import { Main, Section, MainTitle } from "./App.styled";
+import { ContactForm } from "../components/ContactForm/ContactForm";
+import { ContactList } from "../components/ContactList/ContactList";
+import { Filter } from "../components/Filter/Filter";
+import { Title } from "../components/Title/Title";
+
+export default class App extends Component {
+  state = {
+    contacts,
+    filter: "",
+  };
+
+  addContact = ({ name, number }) => {
+    if (
+      this.state.contacts.find(
+        (contact) => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    ) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+    const newContact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  checkContact = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  filterContact = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  removeContact = (contactId) => {
+    this.setState((prev) => ({
+      contacts: prev.contacts.filter((contact) => contact.id !== contactId),
+    }));
+  };
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    if (savedContacts) {
+      this.setState({ contacts: savedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    return (
+      <Main>
+        <MainTitle>Phonebook</MainTitle>
+        <Section>
+          <Title title={"Add contact"} />
+          <ContactForm onSubmit={this.addContact} />
+        </Section>
+        <Section>
+          <Title title="Search contact" />
+          <Filter
+            filterValue={this.state.filter}
+            onChange={this.filterContact}
+          />
+        </Section>
+        <Section>
+          <Title title="Contacts list" />
+          <ContactList
+            contacts={this.checkContact()}
+            onDelete={this.removeContact}
+          />
+        </Section>
+      </Main>
+    );
+  }
+}
---
+++ src/components/Title/Title.jsx
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+import { SectionTitle } from "./Title.styled";
+
+export function Title({ title }) {
+  return <SectionTitle>{title}</SectionTitle>;
+}
+
+Title.propTypes = {
+  title: PropTypes.string.isRequired,
+};
